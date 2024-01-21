e4e57c472a14747907cf58cc5e32812cf8144206
gihon123 <gihonmahendra@gmail.com>
2024-01-21 12:01:43 +0700

    gg1

---
+++ uas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PKKMB DAY 1 "Generasi Inovatif" PKKMB ASIA 2022</title>
+</head>
+<body bgcolor="black">
+    <h1 align="center"><font color ="White">PKKMB DAY 1 "Generasi Inovatif" PKKMB ASIA 2022   </font> 
+    </h1>
+    <left><font size="3" color="red"><i>Humas-FTD | Selasa, 27 September 2022</i></font></left>
+   <img src="https://ftd.asia.ac.id/laravel-filemanager/images/shares/6332ace6a8c38.JPG" alt="Mahasiswa Baru" width="100%">
+    <p align="justify"><font size="4" color="white">PKKMB<b>(Pengenalan Kehidupan Kampus bagi Mahasiswa Baru)</b>  dilaksanakan pada Senin, 26 September 2022 di lapangan Institut Asia. Dengan Opening ceremony yang begitu unik, yaitu jajaran rektorat menaiki Jeep untuk peresmian pembukaan PKKMB ASIA "INOVATIF" 2022. Makna dari tema INOVATIF adalah PKKMB tahun ini dilaksanakan secara Offline setelah 2 tahun berturut-turut dilaksanakan online, dikarenakan PKKMB Offline yang dilakukan pertama setelah pandemi maka diharapkan bisa inovatif. Diikuti kurang lebih 200 mahasiswa PKKMB hari pertama berjalan dengan sangat tertib dan lancar. Semangat generasi INOVATIF 2022, masih ada 4 hari lagi untuk mengikuti rangkaian PKKMB ini </font>  
+    </p>
+    <img src="https://ftd.asia.ac.id/laravel-filemanager/images/shares/6332ace611d69.png" alt="Jeep" width="100%">
+    
+</body>
+</html>
